b9cb49243cd64f0bcdd18b4bff2e1baec4c7fa3b
bmillane <millaneprojection@gmail.com>
2017-02-22 21:37:13 +0000

    Added Tests around Creating and Reading offers made by a User of the website.

---
+++ OrangeBricks.Web.Tests/AggregateTests/OrangeBrickUserTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using NUnit.Framework;
+using OrangeBricks.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OrangeBricks.Web.Tests.ModelTests
+{
+    [TestFixture]
+    public class OrangeBrickUserTests
+    {
+        [Test]
+        public void When_OrangeBrickUser_Makes_Offer_Then_Offer_Added_To_Database()
+        {
+            Mock<IOrangeBricksContext> mockContext = new Mock<IOrangeBricksContext>();
+            mockContext.SetupProperty(x => x.Offers, new Mock<DbSet<Offer>>().Object);
+            OrangeBricksUser User = new OrangeBricksUser(mockContext.Object);
+            Offer offer = new Offer();
+            User.MakeOffer(offer);
+            mockContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void When_OrangeBrickUser_Requests_All_Pending_Offers_List_Then_Pending_Offers_List_Is_Returned()
+        {
+            Mock<IOrangeBricksContext> mockContext = new Mock<IOrangeBricksContext>();
+
+            //This is pretty horrible, not massively experienced with Mocking out Entity Framework classes.
+            var offers = new List<Offer> {
+                new Offer
+                {
+                    Amount = 10,
+                    CreatedAt = DateTime.Now,
+                    Id = 0,
+                    Status = OfferStatus.Pending,
+                    UpdatedAt = DateTime.Now
+                },
+                new Offer
+                {
+                    Amount = 10,
+                    CreatedAt = DateTime.Now,
+                    Id = 0,
+                    Status = OfferStatus.Accepted,
+                    UpdatedAt = DateTime.Now
+                }
+            }.AsQueryable();
+            var offersMock = new Mock<DbSet<Offer>>();
+            offersMock.As<IQueryable<Offer>>().Setup(m => m.Provider).Returns(offers.Provider);
+            offersMock.As<IQueryable<Offer>>().Setup(m => m.Expression).Returns(offers.Expression);
+            offersMock.As<IQueryable<Offer>>().Setup(m => m.ElementType).Returns(offers.ElementType);
+            offersMock.As<IQueryable<Offer>>().Setup(m => m.GetEnumerator()).Returns(offers.GetEnumerator());
+            //End Horribleness
+
+            mockContext.Setup(x => x.Offers).Returns(offersMock.Object);
+            OrangeBricksUser User = new OrangeBricksUser(mockContext.Object);
+            OrangeBrickUserOffersCollection offersCollection = User.GetPendingOffers();
+            Assert.That(offersCollection.Count == 1);
+            Assert.That(offersCollection[0].Status == OfferStatus.Pending);
+        }
+    }
+}
--- OrangeBricks.Web/Models/IdentityModels.cs
+++ OrangeBricks.Web/Models/IdentityModels.cs
@@ -3,6 +3,9 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
 
 namespace OrangeBricks.Web.Models
 {
@@ -16,5 +19,56 @@ namespace OrangeBricks.Web.Models
             // Add custom user claims here
             return userIdentity;
         }
+    }
+
+    //Aggregate Root
+    public class OrangeBricksUser : ApplicationUser
+    {
+        private readonly IOrangeBricksContext context;
+
+        public OrangeBricksUser(IOrangeBricksContext context)
+        {
+            if(context == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            this.context = context;
+        }
+
+        public void MakeOffer(Offer offer)
+        {
+            context.Offers.Add(offer);
+            context.SaveChanges();
+        }
+
+        public OrangeBrickUserOffersCollection GetPendingOffers()
+        {
+            OrangeBrickUserOffersCollection offers = new OrangeBrickUserOffersCollection();
+            foreach (var x in context.Offers)
+            {
+                if(x.Status == OfferStatus.Pending)
+                {
+                    offers.Add(x);
+                }
+            }
+            return offers;
+        }
+
+        public OrangeBrickUserOffersCollection GetAcceptedOffers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrangeBrickUserOffersCollection GetRejectedOffers()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    //Aggregate
+    public class OrangeBrickUserOffersCollection : Collection<Offer>
+    {
     }
 }
